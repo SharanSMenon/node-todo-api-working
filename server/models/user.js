0fe3a4c23d0d8308718bdca7ed2e991edc63ddd1
Sharan Sajiv Menon <sharan@Sharan-MBP.fios-router.home>
2018-10-13 11:08:01 -0400

    Added DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -52,6 +52,14 @@ UserSchema.methods.toJSON = function () {
     return _.pick(userObject, ['_id', 'email'])
 
 }
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+    return user.update({
+        $pull: {
+            tokens: { token }
+        }
+    })
+}
 UserSchema.statics.findByToken = function (token) {
     var User = this;
     var decoded;
@@ -66,16 +74,18 @@ UserSchema.statics.findByToken = function (token) {
         'tokens.access': 'auth'
     })
 }
-UserSchema.statics.findByCredentials = function (email, password){
+UserSchema.statics.findByCredentials = function (email, password) {
     var User = this;
-    return User.findOne({email}).then((user) => {
-        if (!user){
+    return User.findOne({
+        email
+    }).then((user) => {
+        if (!user) {
             return Promise.reject();
         }
         // When user exists
         return new Promise((resolve, reject) => {
             bcrypt.compare(password, user.password, (err, res) => {
-                if (res){
+                if (res) {
                     resolve(user)
                 } else {
                     reject()
